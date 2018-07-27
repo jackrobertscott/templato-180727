9f76244e0282f7a77af5726328519d7c566c9b4d
Jack Scott <jack.rob.scott@gmail.com>
2018-07-27 12:29:29 +0800

    successfully load in the files

--- src/share/containers/App.js
+++ src/share/containers/App.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import GetGist from './GetGist';
-import logo from './logo.svg';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       gistId: null,
+      files: null,
+      loading: false,
     };
   }
 
-  handleGistSubmit(data) {
-    console.log(data);
-    this.setState({ gistId: data });
+  handleGistSubmit({ gistId }) {
+    this.setState({ gistId, loading: true });
+    axios(`https://api.github.com/gists/${gistId}`)
+      .then(({ data }) => {
+        this.setState({ files: Object.values(data.files), loading: false });
+      })
+      .catch(error => console.warn(error));
   }
 
   render() {
-    const { gistId } = this.state;
+    const { gistId, loading, files } = this.state;
     if (!gistId) {
       return (
         <GetGist handleSubmit={(...args) => this.handleGistSubmit(...args)} />
       );
     }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Templato!</h1>
-        </header>
-        <p className="App-intro">Gist id: {gistId}</p>
+      <div>
+        <h1>Welcome to Templato!</h1>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <p>
+            Gist id: {gistId} (files {files.length})
+          </p>
+        )}
       </div>
     );
   }
--- src/share/containers/GetGist.js
+++ src/share/containers/GetGist.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import PropType from 'prop-types';
 import { Formik, Form, Field } from 'formik';
+import { sentenceCase } from 'change-case';
 import * as Yup from 'yup';
-import { Button, InputGroup, ControlGroup } from '@blueprintjs/core';
+import { Button, ControlGroup, Callout } from '@blueprintjs/core';
 import Blurground from '../components/Blurground';
 
 class GetGist extends Component {
@@ -28,9 +29,25 @@ class GetGist extends Component {
           onSubmit={(...args) => this.props.handleSubmit(...args)}
           render={({ isSubmitting, errors }) => (
             <Form>
-              {JSON.stringify(errors, null, 2)}
+              {errors &&
+                !!Object.keys(errors).length && (
+                  <Callout title="Errors" intent="danger">
+                    {Object.values(errors).map(err => (
+                      <span key={err}>
+                        {sentenceCase(err)}
+                        <br />
+                      </span>
+                    ))}
+                  </Callout>
+                )}
+              <br />
               <ControlGroup>
-                <Field type="text" name="gistId" component={InputGroup} />
+                <Field
+                  type="text"
+                  name="gistId"
+                  className="bp3-input"
+                  placeholder="77752bd6787c41fa600b5a5a550dfc9e"
+                />
                 <Button disabled={isSubmitting} type="submit">
                   {isSubmitting ? 'Loading...' : 'Continue'}
                 </Button>
